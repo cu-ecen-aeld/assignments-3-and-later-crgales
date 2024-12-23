665264e4094fbda2f49da6478d54741f0b8d4c5e
crgales <cgales@gmail.com>
2024-12-23 13:35:06 -0500

    Assignment 5 Part 2 - Added start/stop script

---
+++ server/aesdsocket-start-stop.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+case "$1" in
+    start)
+        echo "Starting aesdsocket"
+        start-stop-daemon --start --exec /usr/bin/aesdsocket -- -d
+        ;;
+    stop)
+        echo "Stopping aesdsocket"
+        start-stop-daemon --stop --name aesdsocket
+        ;;
+    *)
+        echo "Usage: $0 {start|stop}"
+        exit 1
+        ;;
+esac
+
+exit 0
