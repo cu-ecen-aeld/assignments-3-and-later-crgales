02c254c92040492e6e6a2e14ce112088006b54e2
crgales <cgales@gmail.com>
2024-12-27 11:24:07 -0500

    Assignment 6 Part 1 - initial

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -6,15 +6,31 @@
 #include <syslog.h>
 #include <fcntl.h>
 #include <signal.h>
+#include <time.h>
+#include <semaphore.h>
+#include <sys/queue.h>
+#include <pthread.h>
 
 #include "aesdsocket.h"
 
 aesdsocket_options_t options;
+file_options_t file_options;
+thread_list_head_t thread_list_head;
 
 void handle_signal(int signal) {
+    thread_list_t *thread_list_entry;
+
     if (signal == SIGINT || signal == SIGTERM) {
         syslog(LOG_INFO, "Received signal %d", signal);
+
         system("rm -f /var/tmp/aesdsocketdata");
+
+        TAILQ_FOREACH(thread_list_entry, &thread_list_head, threads) {
+            pthread_join(thread_list_entry->thread_id, NULL);
+            free(thread_list_entry);
+            thread_list_entry = NULL;
+        }
+
         exit(0);
     }
 }
@@ -51,21 +67,106 @@ void parse_command_line_options(int argc, char *argv[], aesdsocket_options_t *op
     }
 }
 
-void aesdsocket_create_socket(aesdsocket_options_t *options) {
-    int server_fd, file_fd, new_socket;
+void timestamp() {
+    char buffer[BUFFER_SIZE];
+    time_t rawtime;
+    struct tm *timeinfo;
+
+    while (1) {
+        // Acquire the semaphore before accessing the file
+        if (sem_wait(&file_options.semaphore) < 0) {
+            perror("sem_wait");
+            exit(EXIT_FAILURE);
+        }
+
+        time(&rawtime);
+        timeinfo = localtime(&rawtime);
+        strftime(buffer, BUFFER_SIZE, "timestamp:%Y-%m-%d %H:%M:%S\n", timeinfo);
+
+        // Move the file pointer to the end of the file
+        lseek(file_options.file_fd, 0, SEEK_END);
+
+        write(file_options.file_fd, buffer, strlen(buffer));
+        
+        // Release the semaphore after accessing the file  
+        if (sem_post(&file_options.semaphore) < 0) {
+            perror("sem_post");
+            exit(EXIT_FAILURE);
+        }
+        sleep(10);
+    }
+}
+
+void handle_socket(void *arguments) {
+    char buffer[BUFFER_SIZE];  // Allocate a thread-specific buffer
+    int valread;
+    int socket = *((int *)arguments);
+    thread_list_t *thread_list_entry;
+
+    memset(buffer, 0, BUFFER_SIZE);
+    // Reading data from the client
+    while ((valread = read(socket, buffer, BUFFER_SIZE)) > 0) {
+        printf("Received: %s\n", buffer);
+
+        // Acquire the semaphore before accessing the file
+        if (sem_wait(&file_options.semaphore) < 0) {
+            perror("sem_wait");
+            exit(EXIT_FAILURE);
+        }
+
+        // Move the file pointer to the end of the file
+        lseek(file_options.file_fd, 0, SEEK_END);
+
+        // Writing data to the file
+        write(file_options.file_fd, buffer, strlen(buffer));
+        printf("Wrote to the file: %s\n", buffer);
+
+        // Rewind the file pointer to the beginning
+        lseek(file_options.file_fd, 0, SEEK_SET);
+
+        // Clear the buffer
+        memset(buffer, 0, BUFFER_SIZE);
+        
+        // Reading data from the file
+        read(file_options.file_fd, buffer, BUFFER_SIZE);
+        printf("Read from the file: %s\n", buffer);
+
+        // Sending buffer to the client
+        send(socket, buffer, strlen(buffer), 0);
+        printf("\nResponse sent: %s\n", buffer);
+
+        // Release the semaphore after accessing the file  
+        if (sem_post(&file_options.semaphore) < 0) {
+            perror("sem_post");
+            exit(EXIT_FAILURE);
+        }
+
+        // Clear the buffer for the next iteration
+        memset(buffer, 0, BUFFER_SIZE);
+    }
+
+    printf("Client disconnected\n");
+    syslog(LOG_INFO, "Client disconnected");
+
+    // Close the socket and remove the thread from the thread list
+    close(socket);
+    TAILQ_FOREACH(thread_list_entry, &thread_list_head, threads) {
+        if (thread_list_entry->thread_id == pthread_self()) {
+            TAILQ_REMOVE(&thread_list_head, thread_list_entry, threads);
+            free(thread_list_entry);
+            thread_list_entry = NULL;
+            break;
+        }
+    }
+
+}
+
+void aesdsocket_create_socket() {
+    int server_fd, new_socket;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
-    char buffer[BUFFER_SIZE] = {0};
-    int valread;
-
-    openlog("aesdsocket", LOG_PID | LOG_CONS, LOG_USER);
-
-    // Open the file /var/tmp/aesdsocketdata for read/write
-    file_fd = open("/var/tmp/aesdsocketdata", O_RDWR | O_CREAT, 0644);
-    if (file_fd < 0) {
-        perror("open failed");
-        exit(EXIT_FAILURE);
-    }
+    thread_list_t *thread_list_entry;
+    pthread_t thread_id;
 
     // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
@@ -93,7 +194,7 @@ void aesdsocket_create_socket(aesdsocket_options_t *options) {
     }
 
     // Create a daemon if the daemon_mode is set
-    if (options->daemon_mode) {
+    if (options.daemon_mode) {
         daemon(0, 0);
     }
 
@@ -106,45 +207,45 @@ void aesdsocket_create_socket(aesdsocket_options_t *options) {
 
     printf("Server listening on port %d\n", PORT);
     syslog(LOG_INFO, "Server listening on port %d", PORT);
+
     // Accepting incoming connection
     while ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) > 0) {
 
         // Log the accept message to syslog
         syslog(LOG_INFO, "Accepted connection from %s", inet_ntoa(address.sin_addr));
+        printf("Accepted connection from %s\n", inet_ntoa(address.sin_addr));
 
-        // Reading data from the client
-        while ((valread = read(new_socket, buffer, BUFFER_SIZE)) > 0) {
-            printf("Received: %s\n", buffer);
-
-            // Move the file pointer to the end of the file
-            lseek(file_fd, 0, SEEK_END);
-
-            // Writing data to the file
-            write(file_fd, buffer, strlen(buffer));
-
-            // Rewind the file pointer to the beginning
-            lseek(file_fd, 0, SEEK_SET);
-
-            // Clear the buffer
-            memset(buffer, 0, BUFFER_SIZE);
-            
-            // Reading data from the file
-            read(file_fd, buffer, BUFFER_SIZE);
-
-            // Sending buffer to the client
-            send(new_socket, buffer, strlen(buffer), 0);
-            printf("\nResponse sent: %s\n", buffer);
-
-            // Clear the buffer for the next iteration
-            memset(buffer, 0, BUFFER_SIZE);
+        // Create a new thread to handle the socket
+        if (pthread_create(&thread_id, NULL, (void *)handle_socket, (void *)&new_socket) < 0) {
+            perror("pthread_create");
+            close(new_socket);
+            close(server_fd);
+            close(file_options.file_fd);
+            closelog();
+            exit(-1);
         }
 
-        printf("Client disconnected\n");
+        // Create a new thread_list_entry
+        thread_list_entry = (thread_list_t *)malloc(sizeof(thread_list_t));
+        if (thread_list_entry == NULL) {
+            perror("malloc");
+            close(new_socket);
+            close(server_fd);
+            close(file_options.file_fd);
+            closelog();
+            exit(-1);
+        }
+        thread_list_entry->thread_id = thread_id;
+        TAILQ_INSERT_TAIL(&thread_list_head, thread_list_entry, threads);
     }
+    
+    // It is possible that the accept call failed, so close the server socket and exit
+    perror("accept");
+    
     // Closing the socket
     close(new_socket);
     close(server_fd);
-    close(file_fd);
+    close(file_options.file_fd);
     closelog();
 
     system("rm -f /var/tmp/aesdsocketdata");
@@ -153,11 +254,37 @@ void aesdsocket_create_socket(aesdsocket_options_t *options) {
 }
 
 int main(int argc, char *argv[]) {
+    pthread_t thread_id;
 
     parse_command_line_options(argc, argv, &options);
 
+    TAILQ_INIT(&thread_list_head);
+
     setup_signal_handler();
 
+    openlog("aesdsocket", LOG_PID | LOG_CONS, LOG_USER);
+
+    // Open the file /var/tmp/aesdsocketdata for read/write
+    file_options.file_fd = open("/var/tmp/aesdsocketdata", O_RDWR | O_CREAT, 0644);
+    if (file_options.file_fd < 0) {
+        perror("open failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (sem_init(&file_options.semaphore, 0, 1) < 0) {
+        perror("sem_init");
+        exit(EXIT_FAILURE);
+    }
+
+    // Create a thread to write the timestamp to the file
+    if (pthread_create(&thread_id, NULL, (void *)timestamp, NULL) < 0) {
+        perror("pthread_create");
+        close(file_options.file_fd);
+        closelog();
+        exit(EXIT_FAILURE);
+    }
+    
+    // Run sockets in the main thread
     aesdsocket_create_socket(&options);
 
     return 0;
--- server/aesdsocket.h
+++ server/aesdsocket.h
@@ -3,8 +3,16 @@
 
 typedef struct {
     int daemon_mode;
-    int server_fd, file_fd, new_socket;
 } aesdsocket_options_t;
 
-aesdsocket_options_t *aesdsocket_parse_options(int argc, char *argv[]);
-void aesdsocket_create_socket(aesdsocket_options_t *options);
+typedef struct {
+    int file_fd;
+    sem_t semaphore;
+} file_options_t;
+
+typedef struct thread_list {
+    pthread_t thread_id;
+    TAILQ_ENTRY(thread_list) threads;
+} thread_list_t;
+
+typedef TAILQ_HEAD(head_s, thread_list) thread_list_head_t;
